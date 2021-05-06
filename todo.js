a3a871b07186376289978bdc7141e21c1a83aec5
shahnaz <shahnazkhadeeja@gmail.com>
2021-05-06 22:40:45 +0530

    first comment

---
+++ todo.js
@@ -0,0 +1,38 @@
+function display(validate)
+{
+  if(validate(true))
+   {
+       alert("Login Successful!!");
+       window.close();
+      window.open("welcome.html");
+   }
+    validate();
+    
+}
+
+
+function validate(some){
+if(document.getElementById("user").value=="admin" && document.getElementById("inputPassword").value==12345)
+{
+  return true;
+
+    }
+    if(document.getElementById("user").value!="admin" && document.getElementById("inputPassword")==12345){
+            window.alert("Username is Invalid");
+            return false;
+
+    }
+    if(document.getElementById("user").value=="admin" && document.getElementById("inputPassword")!=12345)
+    {
+        alert("password is Invalid");
+        return false;
+
+    }
+    if(document.getElementById("user").value!="admin" && document.getElementById("inputPassword")!=12345){
+            alert("Username and password are Invalid");
+            return false;
+
+    }
+}
+
+//display();
---
+++ todolist.js
@@ -0,0 +1,76 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var jsonobj =  JSON.parse(this.response);
+      
+        var checkboxes= "";
+      var row="";
+      var table="";
+for ( var i=0;i<jsonobj.length; i++) 
+        {
+                   if(jsonobj[i].completed==true)   {
+             row +="<br>"+'<input type="checkbox" class="check" checked disabled>'+(jsonobj[i].title)+"<br>";
+           }
+           else{
+            row +="<br>"+'<input type="checkbox" class="checks">'+(jsonobj[i].title)+"<br>";
+            
+           }
+               
+        document.getElementById("result").innerHTML=row;
+        
+          }  ;
+        }
+      };
+     
+
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+    xhttp.send();
+        }
+
+//   Checking maximum number of tasks should be selected using promise//
+var count=0;
+function counter()
+{
+  
+     count=$(".checks:checked").length;
+   console.log(count);
+   const promise=new Promise((resolve,reject) => {
+    if(count>=95)
+    setTimeout(() => {
+      resolve("You have reached the maximum 5 values!!!");
+   })
+   setTimeout(() => {
+     reject('oops !!');
+   });
+  });
+   promise
+   .then(alert)
+   .catch(console.error);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
